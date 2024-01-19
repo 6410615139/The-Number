1063db6d690810ca4d3a211628ad48914a488c73
Krit <6410615139@cn436>
2024-01-19 13:41:44 +0700

    Initial Commit

---
+++ The Number/The_NumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_NumberApp.swift
+//  The Number
+//
+//  Created by Supakrit Nithikethkul on 19/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct The_NumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
